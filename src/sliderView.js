7d3abe1e9b2ae344ab4d36f5d0e5ba401b3603d0
Martynas <martynas.kazakevicius@gmail.com>
2021-08-25 20:45:26 +0300

    Fixed slider size

--- src/sliderView.js
+++ src/sliderView.js
@@ -82,49 +82,6 @@ class SliderView {
           }
         });
       };
-      //slider();
-
-    // addHandlerRender() {
-    //     const slider = document.querySelector(".slider");
-    //     const slides = document.querySelectorAll(".slide");
-    //     //console.log(slides);
-    //     const btnPrevious = document.querySelector(".slider__btn--left");
-    //     const btnNext = document.querySelector(".slider__btn--right");
-    //     let curSlide = 0;
-    //     const maxSlide = slides.length;
-
-    //     slider.style.overflow = "visible";
-
-    //     const goToSlide = function(slide) {
-    //         slides.forEach((s, i) =>
-    //             (s.style.transform = `translateX(${100 * (i - slide)}%)`));
-    //     };
-
-    //     goToSlide(0);
-
-    //     const nextSlide = function () {
-    //         if (curSlide === maxSlide - 1) {
-    //             curSlide = 0;
-    //         } else {
-    //             curSlide++;
-    //         }
-
-    //         goToSlide(curSlide);
-    //     };
-
-    //     const prevSlide = function () {
-    //         if (curSlide === 0) {
-    //             curSlide = maxSlide - 1;
-    //         } else {
-    //             curSlide--;
-    //         }
-
-    //         goToSlide(curSlide);
-    //     };
-
-    //     btnPrevious.addEventListener('click', nextSlide);
-    //     btnNext.addEventListener('click', prevSlide);
-    // }
 }
 
 export default new SliderView();
